14c0e92de7affda071862c10a8450746ce5b34d4
Beats-Dh <daniel15042014@hotmail.com>
2021-12-06 00:33:59 -0400

    playerSaySpell Optimization || Optimizations and Fixes
    Optimizations and Fixes:
    https://github.com/opentibiabr/optimized_forgottenserver/commit/031cd383f0cb44d152145ba26ef5eecc255460a8#diff-eb01d79b279ebf659ed73216c0b3c490685221c9358e058ce55a1c8ab4e99c35R4896
    
    playerSaySpell Optimization;
    https://github.com/opentibiabr/optimized_forgottenserver/commit/6500e8d5566d6e2504ed0c0505306e8ed2a2c187
    
    Use std::move to move std::string objects when possible instead of doing copies.
    Heavily playerSaySpell optimization up to 50-100x depending on how much spells and talkactions you have.

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -5011,7 +5011,7 @@ void Game::playerSay(uint32_t playerId, uint16_t channelId, SpeakClasses type, c
 		return;
 	}
 
-	if (!text.empty() && text.front() == '/' && player->isAccessPlayer()) {
+	if (text.front() == '/' && player->isAccessPlayer()) {
 		return;
 	}
 
@@ -5062,12 +5062,17 @@ bool Game::playerSaySpell(Player* player, SpeakClasses type, const std::string &
 	}
 
 	std::string words = text;
-	TalkActionResult_t result = g_talkActions().playerSaySpell(player, type, words);
-	if (result == TALKACTION_BREAK) {
-		return true;
+	const std::string &lowerWords = asLowerCaseString(words);
+
+	TalkActionResult_t result;
+	if (text.front() == '/' || text.front() == '!') {
+		result = g_talkActions().playerSaySpell(player, type, lowerWords);
+		if (result == TALKACTION_BREAK) {
+			return true;
+		}
 	}
 
-	result = g_spells().playerSaySpell(player, words);
+	result = g_spells().playerSaySpell(player, words, lowerWords);
 	if (result == TALKACTION_BREAK) {
 		if (!g_configManager().getBoolean(PUSH_WHEN_ATTACKING)) {
 			player->cancelPush();
@@ -5078,11 +5083,9 @@ bool Game::playerSaySpell(Player* player, SpeakClasses type, const std::string &
 		} else {
 			return player->saySpell(type, words, false);
 		}
-
 	} else if (result == TALKACTION_FAILED) {
 		return true;
 	}
-
 	return false;
 }
 
