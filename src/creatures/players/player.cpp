6cbb38237cb16e03a68aa76b4cda6cfdcefaa372
Beats <daniel15042014@hotmail.com>
2023-02-26 18:11:44 -0400

    Replace std::forward_list with std::vector

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -4812,12 +4812,12 @@ double Player::getLostPercent() const {
 
 void Player::learnInstantSpell(const std::string &spellName) {
 	if (!hasLearnedInstantSpell(spellName)) {
-		learnedInstantSpellList.push_front(spellName);
+		learnedInstantSpellList.emplace(spellName);
 	}
 }
 
 void Player::forgetInstantSpell(const std::string &spellName) {
-	learnedInstantSpellList.remove(spellName);
+	learnedInstantSpellList.erase(spellName);
 }
 
 bool Player::hasLearnedInstantSpell(const std::string &spellName) const {
@@ -5365,7 +5365,10 @@ bool Player::hasModalWindowOpen(uint32_t modalWindowId) const {
 }
 
 void Player::onModalWindowHandled(uint32_t modalWindowId) {
-	modalWindows.remove(modalWindowId);
+	auto it = std::find(modalWindows.begin(), modalWindows.end(), modalWindowId);
+	if (it != modalWindows.end()) {
+		modalWindows.erase(it);
+	}
 }
 
 void Player::sendModalWindow(const ModalWindow &modalWindow) {
@@ -5373,7 +5376,7 @@ void Player::sendModalWindow(const ModalWindow &modalWindow) {
 		return;
 	}
 
-	modalWindows.push_front(modalWindow.id);
+	modalWindows.push_back(modalWindow.id);
 	client->sendModalWindow(modalWindow);
 }
 
